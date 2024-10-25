531fd0cb8421794daeb3b2e2fa9efe279c781852
Erin Woo <erinwoo@microsoft.com>
2024-10-25 13:43:32 -0700

    package registration object model

---
+++ NativeAOTDependencyHelper.Core/JsonModels/NuGetPackageRegistration.cs
@@ -0,0 +1,188 @@
+﻿using System.Text.Json.Serialization;
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class NuGetPackageRegistration
+{
+    public string Id { get; set; } = string.Empty;
+    public string[] Type { get; set; } = Array.Empty<string>();
+    public string CommitId { get; set; } = string.Empty;
+    public DateTime CommitTimeStamp { get; set; } = DateTime.MinValue;
+    public int Count { get; set; } = 0;
+    public RegistrationItem[] Items { get; set; } = Array.Empty<RegistrationItem>();
+    public Context Context { get; set; } = new Context();
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class Context
+{
+    public string Vocab { get; set; } = string.Empty;
+    public string Catalog { get; set; } = string.Empty;
+    public string Xsd { get; set; } = string.Empty;
+    public Items Items { get; set; } = new Items();
+    public CommitTimeStamp CommitTimeStamp { get; set; } = new CommitTimeStamp();
+    public CommitId CommitId { get; set; } = new CommitId();
+    public Count Count { get; set; } = new Count();
+    public Parent Parent { get; set; } = new Parent();
+    public Tags Tags { get; set; } = new Tags();
+    public Reasons Reasons { get; set; } = new Reasons();
+    public PackageTargetFrameworks PackageTargetFrameworks { get; set; } = new PackageTargetFrameworks();
+    public DependencyGroups DependencyGroups { get; set; } = new DependencyGroups();
+    public Dependencies Dependencies { get; set; } = new Dependencies();
+    public PackageContent PackageContent { get; set; } = new PackageContent();
+    public Published Published { get; set; } = new Published();
+    public Registration Registration { get; set; } = new Registration();
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class Items
+{
+    public string Id { get; set; } = string.Empty;
+    public string Container { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class CommitTimeStamp
+{
+    public string Id { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class CommitId
+{
+    public string Id { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class Count
+{
+    public string Id { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class Parent
+{
+    public string Id { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class Tags
+{
+    public string Id { get; set; } = string.Empty;
+    public string Container { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class Reasons
+{
+    public string Container { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class PackageTargetFrameworks
+{
+    public string Id { get; set; } = string.Empty;
+    public string Container { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class DependencyGroups
+{
+    public string Id { get; set; } = string.Empty;
+    public string Container { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class Dependencies
+{
+    public string Id { get; set; } = string.Empty;
+    public string Container { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class PackageContent
+{
+    public string Type { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class Published
+{
+    public string Type { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class Registration
+{
+    public string Type { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class RegistrationItem
+{
+    public string Id { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string CommitId { get; set; } = string.Empty;
+    public DateTime CommitTimeStamp { get; set; } = DateTime.MinValue;
+    public int Count { get; set; } = 0;
+    public Item[] Items { get; set; } = Array.Empty<Item>();
+    public string Parent { get; set; } = string.Empty;
+    public string Lower { get; set; } = string.Empty;
+    public string Upper { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class Item
+{
+    public string Id { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string CommitId { get; set; } = string.Empty;
+    public DateTime CommitTimeStamp { get; set; } = DateTime.MinValue;
+    public CatalogEntry CatalogEntry { get; set; } = new CatalogEntry();
+    public string PackageContent { get; set; } = string.Empty;
+    public string Registration { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class CatalogEntry
+{
+    public string IdUrl { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string Authors { get; set; } = string.Empty;
+    public DependencyGroup[] DependencyGroups { get; set; } = Array.Empty<DependencyGroup>();
+    public string Description { get; set; } = string.Empty;
+    public string IconUrl { get; set; } = string.Empty;
+    public string Id { get; set; } = string.Empty;
+    public string Language { get; set; } = string.Empty;
+    public string LicenseExpression { get; set; } = string.Empty;
+    public string LicenseUrl { get; set; } = string.Empty;
+    public bool Listed { get; set; } = false;
+    public string MinClientVersion { get; set; } = string.Empty;
+    public string PackageContent { get; set; } = string.Empty;
+    public string ProjectUrl { get; set; } = string.Empty;
+    public DateTime Published { get; set; } = DateTime.MinValue;
+    public bool RequireLicenseAcceptance { get; set; } = false;
+    public string Summary { get; set; } = string.Empty;
+    public string[] Tags { get; set; } = Array.Empty<string>();
+    public string Title { get; set; } = string.Empty;
+    public string Version { get; set; } = string.Empty;
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class DependencyGroup
+{
+    public string Id { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string TargetFramework { get; set; } = string.Empty;
+    public Dependency[] Dependencies { get; set; } = Array.Empty<Dependency>();
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Skip)]
+public class Dependency
+{
+    public string IdUrl { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string Range { get; set; } = string.Empty;
+    public string Registration { get; set; } = string.Empty;
+}
