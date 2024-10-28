7cc5d6abf9a2ba4611cbaf341c80c1f4d9940dc7
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2024-10-28 15:05:58 -0700

    PR Feedback - Convert to File-scoped namespace for DetailsControl

--- NativeAOTDependencyHelper.App/Controls/DetailsControl.xaml.cs
+++ NativeAOTDependencyHelper.App/Controls/DetailsControl.xaml.cs
@@ -1,34 +1,21 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using NativeAOTDependencyHelper.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
-namespace NativeAOTDependencyHelper.App.Controls
+namespace NativeAOTDependencyHelper.App.Controls;
+
+public sealed partial class DetailsControl : UserControl
 {
-    public sealed partial class DetailsControl : UserControl
+    public DetailsControl()
     {
-        public DetailsControl()
-        {
-            this.InitializeComponent();
-        }
+        this.InitializeComponent();
+    }
 
-        public static readonly DependencyProperty SelectedPackageProperty = DependencyProperty.Register(nameof(SelectedPackage), typeof(NuGetPackageViewModel), typeof(DetailsControl), new PropertyMetadata(defaultValue: null));
+    public static readonly DependencyProperty SelectedPackageProperty = DependencyProperty.Register(nameof(SelectedPackage), typeof(NuGetPackageViewModel), typeof(DetailsControl), new PropertyMetadata(defaultValue: null));
 
-        public NuGetPackageViewModel SelectedPackage
-        {
-            get => (NuGetPackageViewModel)GetValue(SelectedPackageProperty);
-            set => SetValue(SelectedPackageProperty, value);
-        }
+    public NuGetPackageViewModel SelectedPackage
+    {
+        get => (NuGetPackageViewModel)GetValue(SelectedPackageProperty);
+        set => SetValue(SelectedPackageProperty, value);
     }
 }
