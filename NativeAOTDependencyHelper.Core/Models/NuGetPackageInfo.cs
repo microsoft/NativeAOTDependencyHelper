359643eb2034fc4b10140acc93d821e34d2f58fb
Michael Hawker <24302614+michael-hawker@users.noreply.github.com>
2024-10-29 01:15:56 -0700

    Split out Project Info from the Package, a Package can now have multiple project references
    This lets us just have a single high-level view of the packages vs. having them duplicated in the main list.
    This also means we won't duplicate reports/checks that don't rely on Framework information (and they can check that all at once against the list of frameworks for projects for that package anyway this way).

--- NativeAOTDependencyHelper.Core/Models/NuGetPackageInfo.cs
+++ NativeAOTDependencyHelper.Core/Models/NuGetPackageInfo.cs
@@ -6,21 +6,14 @@ namespace NativeAOTDependencyHelper.Core.Models;
 /// Core base class representing base-level nuget package info from solution file.
 /// Remapped from the Json data provided by <see cref="DotnetPackageList"/>.
 /// </summary>
-/// <param name="ParentProjectPath">The filepath to the project that uses this package.</param>
 /// <param name="Name">The <see cref="NuGetPackage.Id"/> of this package.</param>
-/// <param name="Framework">The target framework this package is evaluating for.</param>
-/// <param name="RequestedVersion">The version which the project requested for this package.</param>
-/// <param name="ResolvedVersion">The version NuGet decided to use for this package.</param>
-public record NuGetPackageInfo(string ParentProjectPath,
-                               string Name,
-                               string Framework,
-                               string RequestedVersion,
-                               string ResolvedVersion)
+/// <param name="ProjectReferences">List of projects which reference this package.</param> // TODO: This could be a problem being a record-type if we add additional references here by transitive dependencies...
+public record NuGetPackageInfo(string Name, NuGetPackageProjectReference[] ProjectReferences)
 {
     /// <summary>
-    /// Gets if this package is a transitive reference and required for a referenced package. If <c>false</c>, then the package explicitly requested to use this package.
+    /// Gets whether this NuGetPackage is ONLY included due to transitive references and has no direct references by projects.
     /// </summary>
-    public bool IsTransitive { get; init; }
+    public bool IsTransitiveOnly => ProjectReferences.All(p => p.IsTransitive);
 
     /// <summary>
     /// Takes the Json data from <see cref="DotnetPackageList"/> and flattens into a straight list of <see cref="NuGetPackageInfo"/> for passing to <see cref="IReportItemProvider"/>.
@@ -37,33 +30,40 @@ public record NuGetPackageInfo(string ParentProjectPath,
             throw new IOException("Invalid project path. Please use a valid .sln file");
         }
 
+        Dictionary<string, List<NuGetPackageProjectReference>> _uniquePackageIndex = new();
+
         foreach (var project in packageList.Projects)
         {
             foreach (var framework in project.Frameworks)
             {
-                foreach (var package in framework.TopLevelPackages)
+                // Layer is either TopLevel (0) or Transitive (1)
+                for (int layer = 0; layer <= 1; layer++)
                 {
-                    yield return new NuGetPackageInfo(
-                        ParentProjectPath: project.Path.Substring(commonPathIndex),
-                        Name: package.Id,
-                        Framework: framework.Framework,
-                        RequestedVersion: package.RequestedVersion,
-                        ResolvedVersion: package.ResolvedVersion);
-                }
-
-                foreach (var package in framework.TransitivePackages)
-                {
-                    yield return new NuGetPackageInfo(
-                        ParentProjectPath: project.Path.Substring(commonPathIndex),
-                        Name: package.Id,
-                        Framework: framework.Framework,
-                        RequestedVersion: package.RequestedVersion,
-                        ResolvedVersion: package.ResolvedVersion)
+                    // See Switch Expression: https://learn.microsoft.com/dotnet/csharp/language-reference/operators/switch-expression
+                    foreach (var package in layer switch { 0 => framework.TopLevelPackages, 
+                                                           1 => framework.TransitivePackages, 
+                                                           _ => throw new IndexOutOfRangeException() })
                     {
-                        IsTransitive = true
-                    };
+                        if (!_uniquePackageIndex.ContainsKey(package.Id))
+                        {
+                            _uniquePackageIndex[package.Id] = new();
+                        }
+
+                        _uniquePackageIndex[package.Id].Add(new NuGetPackageProjectReference(
+                            ParentProjectPath: project.Path.Substring(commonPathIndex),
+                            Framework: framework.Framework,
+                            RequestedVersion: package.RequestedVersion,
+                            ResolvedVersion: package.ResolvedVersion,
+                            TransitiveLayer: layer));
+                    }
                 }
             }
+        }
+
+        // Construct final package info with references to all projects used
+        foreach ((var packageId, var projects) in _uniquePackageIndex)
+        {
+            yield return new NuGetPackageInfo(packageId, projects.ToArray());
         }
     }
 
---
+++ NativeAOTDependencyHelper.Core/Models/NuGetPackageProjectReference.cs
@@ -0,0 +1,21 @@
+﻿namespace NativeAOTDependencyHelper.Core.Models;
+
+/// <summary>
+/// Represents a .NET Project's reference to a NuGet package, contained within <see cref="NuGetPackageInfo"/>.
+/// </summary>
+/// <param name="ParentProjectPath">The filepath to the project that uses this package.</param>
+/// <param name="Framework">The target framework this package is evaluating for.</param>
+/// <param name="RequestedVersion">The version which the project requested for this package.</param>
+/// <param name="ResolvedVersion">The version NuGet decided to use for this package.</param>
+/// <param name="TransitiveLayer">How many levels removed is this package from the top-level packages for this project? If <c>zero</c>, it is a top-level reference directly included by a project.</param>
+public record NuGetPackageProjectReference(string ParentProjectPath,
+                                           string Framework,
+                                           string RequestedVersion,
+                                           string ResolvedVersion,
+                                           int TransitiveLayer)
+{
+    /// <summary>
+    /// Gets whether this is a transitive reference or not (dependency of another directly referenced package). <c>true</c> when <see cref="TransitiveLayer"/> is non-zero.
+    /// </summary>
+    public bool IsTransitive => TransitiveLayer > 0;
+}
